99ac4b4d81f178c56bbae79947c3479e4bb7b2d3
jambupink <Latiff0601@gmail.com>
2025-02-12 08:04:14 +0800

    test

--- WebApplication13/Program.cs
+++ WebApplication13/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.AddDbContext<AuthDbContext>();
+
 builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<AuthDbContext>();
 
 var app = builder.Build();
